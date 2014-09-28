aa259ea2531adc70d167a22ce906bacecc6a2059
Jose Ricaurte <guitarguy311@yahoo.com>
2014-09-27 18:10:46 -0600

    Made homepage and assignments page

---
+++ assignmentsPage.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Jose Ricaurte's Homepage</title>
+  <link rel="stylesheet" type="text/css" href="cs313homepage.css">
+</head>
+
+<body>
+		<h1>Coming Soon!</h1>
+	
+</body>
+</html>
